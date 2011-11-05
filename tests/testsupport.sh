1cf4d50a551f88ad61c9cf7764b435e8531172df
Simon Sadedin <simon.sadedin@mcri.edu.au>
2011-11-05 15:26:30 +1100

    added missing support file for running tests

---
+++ tests/testsupport.sh
@@ -0,0 +1,28 @@
+# Add BPipe from the project root to the path
+if [ -z "$BASE" ];
+then
+	BASE=`pwd`
+	if [ ! -e $BASE/../bin/bpipe ] && [ -e $BASE/../../bin/bpipe ] ;
+	then
+		BASE=`pwd`/..
+	fi
+fi
+
+PATH="$BASE/../bin:$PATH"
+
+function err() {
+	echo
+	echo "ERROR: $1"
+	echo
+	exit 1
+}
+
+# Convenience function to run the test
+function run() {
+	bpipe run test.groovy $* > test.out 2>&1
+}
+
+# Convenience function to run in test mode
+function runtest() {
+	bpipe test test.groovy $* > test.out 2>&1
+}
