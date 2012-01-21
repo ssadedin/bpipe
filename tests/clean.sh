01fdb9be45dbca9f40d70bd97e52f067727dd94f
Simon Sadedin <ssadedin@badboy.com.au>
2012-01-21 12:22:37 +1100

    added support for individual cleanup scripts for tests Removed unwanted log file

--- tests/clean.sh
+++ tests/clean.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+# Clean up standard unwanted files
 for i in .bpipe test.out commandlog.txt ;
 do
 	find . -name "$i" | xargs rm -rf
 done
+
+# Run individual cleanup scripts
+for i in `find . -name cleanup.sh`; do cd `dirname $i`; ./cleanup.sh; cd - ; done
+
