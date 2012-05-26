cd41a7cb9d3d4b8be3840cd0c5fa78c030d495a6
Simon Sadedin <ssadedin@badboy.com.au>
2012-05-26 13:45:16 +1000

    fixed test with incorrect checks

--- tests/transform_multi/run.sh
+++ tests/transform_multi/run.sh
@@ -7,8 +7,8 @@ run test.txt
 grep -q "Stage hello" test.out || err "Failed to find expected stage hello"
 grep -q "Stage world" test.out || err "Failed to find expected stage world"
 
-[ ! -f test.f1.txt ] && err "Failed to find expected output test.f1.txt"
-[ ! -f test.f1.f2.txt ] && err "Failed to find expected output test.f1.f2.txt"
-[ ! -f test.f1.f3.txt ] && err "Failed to find expected output test.f2.f3.txt"
+[ ! -f test.f1 ] && err "Failed to find expected output test.f1"
+[ ! -f test.f2 ] && err "Failed to find expected output test.f2"
+[ ! -f test.f3 ] && err "Failed to find expected output test.f3"
 
 true
