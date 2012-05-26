5535234428f33d219430fb73e18757cea4b3dba6
Simon Sadedin <ssadedin@badboy.com.au>
2012-05-26 13:14:43 +1000

    fixed bad check in documentation test

--- tests/documentation/run.sh
+++ tests/documentation/run.sh
@@ -8,7 +8,7 @@ grep -q "Stage hello" test.out || err "Failed to find expected stage hello"
 grep -q "Stage bar" test.out || err "Failed to find expected stage world"
 grep -q "Stage fail" test.out || err "Failed to find expected stage world"
 
-[ ! -e doc/index.html ] || err "Unable to find generated documentation"
+[ -e doc/index.html ] || err "Unable to find generated documentation"
 
 grep -q "This pipeline is only" doc/index.html || err "Unable to find description for foo stage in index.html"
 grep -q "Fails Every Time" doc/index.html || err "Unable to find title for fail stage in index.html"
