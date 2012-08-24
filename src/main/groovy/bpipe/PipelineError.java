1dd36f8c4914542ae2625de692302210c33f35ee
Simon Sadedin <ssadedin@badboy.com.au>
2012-08-24 12:05:41 +1000

    Issue 61: Errors when running Bpipe with JDK7

--- src/main/groovy/bpipe/PipelineError.java
+++ src/main/groovy/bpipe/PipelineError.java
@@ -22,7 +22,7 @@
 * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
 * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-package bpipe
+package bpipe;
 
 /**
  * A user level error that occurred during pipeline execution.
--- src/main/groovy/bpipe/PipelineTestAbort.java
+++ src/main/groovy/bpipe/PipelineTestAbort.java
@@ -23,7 +23,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package bpipe
+package bpipe;
 
 /**
  * This exception is thrown when the pipeline is aborted because
