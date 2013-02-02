a9bb065365b216cad2562dff5623826c021bcafa
Simon Sadedin <ssadedin@badboy.com.au>
2013-02-02 12:56:57 +1100

    migration to groovy 2.1 (test)

--- src/main/groovy/bpipe/Runner.groovy
+++ src/main/groovy/bpipe/Runner.groovy
@@ -31,6 +31,7 @@ import java.util.logging.Handler;
 import java.util.logging.Level
 import java.util.logging.Logger;
 
+import groovy.transform.CompileStatic;
 import groovy.util.logging.Log
 
 /**
@@ -460,7 +461,7 @@ class Runner {
  *
  */
 @Log
-private class ParamsBinding extends Binding {
+class ParamsBinding extends Binding {
     
     def parameters = []
 
