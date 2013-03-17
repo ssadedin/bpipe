8e48d85045a317e8d9806573ecc7d6fc15ace430
Simon Sadedin <ssadedin@badboy.com.au>
2013-03-17 15:14:40 +1100

    made test more flexible to allow for allowable variation in outputs used

--- tests/double_parallel/run.sh
+++ tests/double_parallel/run.sh
@@ -12,6 +12,6 @@ grep -q "Stage end" test.out || err "Failed to find expected stage end"
 [ ! -f test.txt.hello ] && err "Failed to find expected output test.txt.hello"
 [ ! -f test.txt.hello.world ] && err "Failed to find expected output test.txt.hello.world"
 [ ! -f test.txt.hello.world2 ] && err "Failed to find expected output test.txt.hello.world2"
-[ ! -f test.txt.hello.world.how_are_you.end ] && err "Failed to find expected output test.txt.hello.world.how_are_you.end"
+[ ! -f test.txt.hello.world*.*.end ] && err "Failed to find expected output test.txt.hello.world.how_are_you.end"
 
 true
