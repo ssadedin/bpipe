282704bb03217b29eb4b79540d34f5c27c7ef313
Simon Sadedin <simon.sadedin@mcri.edu.au>
2013-04-21 09:33:56 +1000

    removed test for removed function

--- tests/produce_override_output_dir/run.sh
+++
@@ -1,9 +0,0 @@
-source ../testsupport.sh
-
-run test.txt
-
-grep -q "could not be found" test.out && err "Incorrectly reported that output in non-default directory could not be found"
-
-exists outdir/test.csv
-
-true
