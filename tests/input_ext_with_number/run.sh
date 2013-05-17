d0fa8a2ca57627098c250ed420671c523b592dc8
Simon Sadedin <ssadedin@gmail.com>
2013-05-17 11:52:56 -0700

    fix test to work with slightly modified behavior regarding which input is used as base for output when multiple are referenced

--- tests/input_ext_with_number/run.sh
+++ tests/input_ext_with_number/run.sh
@@ -2,9 +2,9 @@ source ../testsupport.sh
 
 run test1.txt  test1.csv test2.txt 
 
-exists test2.hello.xml
+exists test1.hello.xml
 
-[ `cat test2.hello.xml | groovy -e 'print(System.in.readLines()[0..1] == ["2","3"])'` == true ] || err "Contents of output file test2.hello.xml didn't match expected"
+[ `cat test1.hello.xml | groovy -e 'print(System.in.readLines()[0..1] == ["2","3"])'` == true ] || err "Contents of output file test1.hello.xml didn't match expected"
 
 true
 
