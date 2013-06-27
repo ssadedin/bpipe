4014dea234ed13a91e602db563ae36876865881b
Simon Sadedin <simon.sadedin@mcri.edu.au>
2013-06-27 13:58:59 +1000

    made test check outputs more in depth since correct files were being created but with incorrect contents

--- tests/nested_parallel/run.sh
+++ tests/nested_parallel/run.sh
@@ -13,4 +13,8 @@ grep -q "Stage end" test.out || err "Failed to find expected stage end"
 [ ! -f test.txt.hello.take_me_to_your_leader ] && err "Failed to find expected output test.txt.hello.take_me_to_your_leader"
 [ ! -f test.txt.hello.how_are_you ] && err "Failed to find expected output test.txt.hello.how_are_you"
 
+grep -q "goo" *.end || err "Failed to find goo in output as expected from test.txt.hello.take_me_to_your_leader"
+grep -q "foo" *.end || err "Failed to find foo in output as expected from test.txt.hello"
+grep -q "boo" *.end || err "Failed to find boo in output as expected from test.txt.hello.how_are_you"
+
 true
