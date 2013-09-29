1f872cd7c8d9a43ac587f91503fe1d7036747219
Simon Sadedin <simon.sadedin@mcri.edu.au>
2013-09-29 19:49:54 +1000

    fixed test to match behavior of new filter rule for inferring output

--- tests/filter_multi_prior_input/run.sh
+++ tests/filter_multi_prior_input/run.sh
@@ -2,7 +2,12 @@ source ../testsupport.sh
 
 run test1.txt test2.txt
 
-exists test1.hello.goo.txt
+# Note: this used to be test1.hello.goo.txt, 
+# but since the output is not derived from any input passing through the 
+# 'hello' stage, it actually makes more sense to be test1.goo.txt.
+# That change happened when another bug related to filters 
+# was fixed - see filter_uses_correct_base_for_output test.
+exists test1.goo.txt
 
 grep -q "failed" test.out && err "Pipeline failed but should have succeeded"
 
