3d30727188359bb86ed540b7949c53f3ee02c60f
Simon Sadedin <ssadedin@gmail.com>
2013-11-14 12:29:51 +1100

    add missing run.sh for test chnage test to expect slightly modified output for special case of $output2 referenced without $output1

--- tests/output_ext_with_number/run.sh
+++ tests/output_ext_with_number/run.sh
@@ -3,6 +3,16 @@ source ../testsupport.sh
 run test.txt
 
 exists test.hello.csv
-exists test.hello.world.csv
+
+# Note: used to be test.hello.world.csv
+# However the refernece to $output2.csv without $output1 referenced is difficult to handle.
+# we used to default to naming $output2 the same as $output1 if $output1 was not referenced
+# anywhere. However this is tricky because we might see the references out of order - 
+# $output2 first and then $output1. We can't know at the time $output2 is referenced whether
+# $output1 is going to be referenced after. So without some kind of 2-phase property resolution
+# process we have to assume $output1 might get referenced. Thus we now change to pre-emptively
+# name $output2 on the assumption that we might see $output1 later on - ie: it gets a 
+# numeric offset inserted, if it would end up the same name as $output1.
+exists test.hello.2.world.csv
 
 true
