03a00986df1f9e6a6023cb09bac2ef5a6a11976c
Simon Sadedin <simon.sadedin@mcri.edu.au>
2013-11-14 13:50:19 +1100

    fix R code failing when last line contains certain syntax

--- src/main/groovy/bpipe/PipelineContext.groovy
+++ src/main/groovy/bpipe/PipelineContext.groovy
@@ -1237,6 +1237,10 @@ class PipelineContext {
      * @param scr
      */
     void R(Closure c) {
+        R(c,null)
+    }
+    
+    void R(Closure c, String config) {
         log.info("Running some R code")
         
         // When probing, just evaluate the string and return
@@ -1265,7 +1269,8 @@ class PipelineContext {
             String scr = c()
             exec("""unset TMP; unset TEMP; TEMPDIR="$rTempDir" Rscript - <<'!'
             $scr
-!""",false)
+!
+""",false, config)
        }
        finally {
            this.echoWhenNotFound = oldEchoFlag
