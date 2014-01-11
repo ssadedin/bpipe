72be981e5806c1d934c71be3006568b265a25ec6
Simon Sadedin <simon.sadedin@mcri.edu.au>
2014-01-11 12:45:22 +1100

    fix incorrect assertion in test

--- tests/filter_on_prev_input/run.sh
+++ tests/filter_on_prev_input/run.sh
@@ -4,6 +4,6 @@ run test.txt
 
 grep -q 'test.hello.there.vep.csv' test.out && err "Output contains reference to incorrect file, test.hello.there.vep.csv"
 
-exists test.hello.vep.vcf
+exists variants/test.hello.vep.vcf
 
 true
