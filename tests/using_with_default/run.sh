5331bcf73cd0a41b007fa7ea387394ce7196463b
Simon Sadedin <simon.sadedin@mcri.edu.au>
2014-02-22 22:30:12 +1100

    fix test failing due to rewording of output

--- tests/using_with_default/run.sh
+++ tests/using_with_default/run.sh
@@ -4,6 +4,6 @@ run
 
 grep -q "x = 15 and y = 20" test.out || err "Did not find expected values x = 15 and y = 20 in output"
 
-grep -q "requires a parameter y" test.out || err "Did not find expected text 'requires a parameter y' in output"
+grep -q "Variable or parameter 'x' was not specified" test.out || err "Did not find expected text 'Variable or parameter 'x' was not specified' in output"
 
 true
