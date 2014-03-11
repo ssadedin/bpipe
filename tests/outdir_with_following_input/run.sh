810e0e8fd22266ca2b87e1ad8350976b0e735c85
Simon Sadedin <simon.sadedin@mcri.edu.au>
2014-03-11 11:16:27 +1100

    prevent output showing in test

--- tests/outdir_with_following_input/run.sh
+++ tests/outdir_with_following_input/run.sh
@@ -1,6 +1,6 @@
 source ../testsupport.sh
 
-bpipe run -d outdir test.groovy test.txt
+bpipe run -d outdir test.groovy test.txt > test.out 2>&1
 
 exists outdir/hello.output.world.xml outdir/hello.output.csv
 
