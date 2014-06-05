6f22d9fe984190ad4293b577643be8f54c3b1b00
Simon Sadedin <ssadedin@gmail.com>
2014-06-05 22:19:51 +1000

    support for PBS Pro thanks to Davide Rambaldi

---
+++ bin/bpipe-pbspro.sh
@@ -0,0 +1,253 @@
+#!/bin/bash
+
+# Start, stop and get status of jobs running on a PBS PROFESSIONAL job scheduler.
+#
+# Usage:
+#
+# Starting a job (will print job ID on standard output):
+#
+#    COMMAND="foobar" NAME=test WALLTIME="00:01:00" PROCS=1 QUEUE=batch ./bpipe-torque.sh start
+#
+# Stopping a job (given some job id "my_job_id")
+#
+#    ./bpipe-torque.sh stop my_job_id
+#
+# Getting the status of a job (given some job id "my_job_id")
+#
+#    ./bpipe-torque.sh status my_job_id
+#
+# Notes:
+#
+# None of the commands are guaranteed to succeed. An exit status of 0 for this script
+# indicates success, all other exit codes indicate failure (see below).
+#
+# Stopping a job may not cause it to stop immediately. You are advised to check the
+# status of a job after asking it to stop. You may need to poll this value.
+#
+# We are not guaranteed to know the exit status of a job command, for example if the
+# job was killed before the command was run.
+#
+# Inspired by bpipe-torque.sh from: Bernie Pope, Simon Sadedin, Alicia Oshlack
+# Author: Davide Rambaldi <davide.rambaldi AT gmail DOT com>
+# Copyright 2013.
+
+# This is what we call the program in user messages
+program_name=bpipe-pbspro
+
+# exit codes:
+SUCCESS=0
+INCORRECT_FIRST_ARGUMENT=1 # must be start, stop, or status
+MISSING_JOB_PARAMETER=2    # one of the env vars not defined
+STOP_MISSING_JOBID=3       # stop command not given job id as parameter
+STATUS_MISSING_JOBID=4     # status command not given job id as parameter
+QDEL_FAILED=5              # qdel command returned non-zero exit status
+QSTAT_FAILED=6             # qstat command returned non-zero exit status
+QSUB_FAILED=7              # qsub command returned non-zero exit status
+MKDIR_JOBDIR_FAILED=8      # mkdir $JOBDIR failed
+
+ESSENTIAL_ENV_VARS="COMMAND NAME PBSOUTPUT PBSERROR"
+OPTIONAL_ENV_VARS="WALLTIME PROCS QUEUE JOBDIR MEMORY"
+DEFAULT_BATCH_MEM=1
+DEFAULT_BATCH_PROCS=1
+DEFAULT_QUEUE=workq
+
+# Print a usage message
+usage () {
+	echo "usage: $program_name (start | stop ID | status ID)"
+	echo "start needs these environment variables: $ESSENTIAL_ENV_VARS"
+	echo "start will use these variables if defined: $OPTIONAL_ENV_VARS"
+}
+
+# Generate a PBS script from parameters found in environment variables.
+make_pbs_script () {
+	# check that all the essential environment variables are defined
+	for v in $ESSENTIAL_ENV_VARS; do
+		eval "k=\$$v" 
+		if [[ -z $k ]]; then
+			echo "$program_name ERROR: environment variable $v not defined"
+			echo "these environment variables are required: $ESSENTIAL_ENV_VARS"
+			exit $MISSING_JOB_PARAMETER
+		fi
+	done
+
+	# set the queue
+	if [[ -z $QUEUE ]]; then
+		QUEUE=$DEFAULT_QUEUE
+	fi
+
+	# set the job directory if needed
+	if [[ -n $JOBDIR ]]; then
+		# check if the directory already exists
+		if [[ ! -d "$JOBDIR" ]]; then
+			# try to make the directory
+			mkdir "$JOBDIR"
+			# check if the mkdir succeeded
+			if [[ $? != 0 ]]; then
+				echo "$program_name ERROR: could not create job directory $JOBDIR"
+				exit $MKDIR_JOBDIR_FAILED
+			fi
+		fi
+		job_script_name="$JOBDIR/job.pbs"
+	else
+		job_script_name="job.pbs"
+	fi
+	
+	if [[ ! -z $PROJECT ]]; then
+		project_name="#PBS -P ${PROJECT}"
+	fi
+
+	if [[  ! -z $ACCOUNT ]]; then
+		account="#PBS -A $ACCOUNT"
+	fi
+
+	# cfr man pbs_resources
+	# : Format:
+    #       qsub ... -l <resource name>=<value>   <--- job-wide request
+    #                -l select=<chunks>           <--- selection statement
+    #   The only resources that can be requested in chunks are host-level resources, such as mem and ncpus.  
+    #   The only resources that can be in a job-wide request are server-level or queue-level resources, such as walltime.  
+
+    # we have a SELECT_STATEMENT var
+    if [[ ! -z $SELECT_STATEMENT ]]; then
+    	select_request="#PBS -l $SELECT_STATEMENT"
+    fi
+
+	if [[ ! -z $WALLTIME ]]; then
+		walltime_request="#PBS -l walltime=${WALLTIME}"
+	fi
+
+	# First strip name beacuse in PBS pro -N name have the following specs:
+	# Format: string, up to 15  characters  in  length.   
+	# It must consist of an alphabetic or numeric character 
+	# followed by printable, non-white-space characters.
+	TRIMMED_NAME=$(echo $NAME | cut -c 1-15)
+
+	# write out the job script to a file
+	cat > $job_script_name << HERE
+#!/bin/bash
+#PBS -N $TRIMMED_NAME
+#PBS -q $QUEUE
+#PBS -o $PBSOUTPUT
+#PBS -e $PBSERROR
+$project_name
+$account
+$memory_request
+$procs_request
+$walltime_request
+$select_request
+
+cd \$PBS_O_WORKDIR
+$COMMAND
+HERE
+
+	echo $job_script_name
+}
+
+# Launch a job on the queue.
+start () {
+	# create the job script
+	job_script_name=`make_pbs_script`
+	# check that the job script file exists
+	if [[ -f $job_script_name ]]
+	then
+		# launch the job and get its id
+		job_id_full=`qsub $job_script_name`
+		qsub_exit_status=$?
+		if [[ $? -eq 0 ]]
+		then
+			# bite off the job number from the start of the job identifier
+			job_id_number=`echo $job_id_full | sed -n 's/\([0-9][0-9]*\).*/\1/p'`
+			echo $job_id_number
+		else
+			echo "$program_name ERROR: qsub returned non zero exit status $qsub_exit_status"
+			exit $QSUB_FAILED
+		fi
+	else
+		echo "$program_name ERROR: could not create job script $job_script_name"
+	fi
+}
+
+# stop a job given its id
+# XXX should we check the status of the job first?
+stop () {
+	# make sure we have a job id on the command line
+	if [[ $# -ge 1 ]]
+	then
+		# try to stop it
+		qdel "$1"
+		qdel_success=$?
+		if [[ qdel_success == 0 ]]
+		then
+			exit $SUCCESS
+		else
+			exit $QDEL_FAILED
+		fi
+	else
+		echo "$program_name ERROR: stop requires a job identifier"
+		exit $STOP_MISSING_JOBID
+	fi
+}
+
+# get the status of a job given its id
+status () {
+	# make sure we have a job id on the command line
+	if [[ $# -ge 1 ]]
+	then
+		# look at the output of qstat
+		# Davide Rambaldi: adding -x for PBS Professional engine
+		qstat_output=`qstat -x -f "$1"`
+		qstat_success=$?
+		if [[ $qstat_success == 0 ]]
+		then
+			# XXX what to do if the awk fails?
+			job_state=`echo "$qstat_output" | awk '/job_state =/ { print $3 }'`
+			case "$job_state" in
+				Q|H|W) echo WAITING;;   # job is in Queue or on Hold or Waiting for start time to arrive
+				R|E) echo RUNNING;;     # if the job is exiting (E) will still think it is running
+									    # XXX what to do if the awk fails?
+				F) command_exit_status=`echo "$qstat_output" | awk '/Exit_status =/ { print $3 }'`
+										# Davide Rambaldi: adding cases for PBS Professional engine: Finished status
+										# is F in PBS professional and exit_status is Exit_status.
+					echo "COMPLETE $command_exit_status";;
+				*) echo UNKNOWN;;  		# there are other codes such as:
+								        #   T (job being moved) 
+			esac
+			exit $SUCCESS
+			# it seems if qstat doesn't know about the job id it returns 153
+			# this can happen on a legitimate job id when qstat decides that
+			# the job is too old to remember about
+		elif [[ $qstat_success == 153 ]]
+		then
+			echo UNKNOWN
+			# all other qstat errors are treated as failures
+		else
+			exit $QSTAT_FAILED
+		fi
+	else
+		echo "$program_name ERROR: status requires a job identifier"
+		exit $STATUS_MISSING_JOBID
+	fi
+}
+
+# run the whole thing
+main () {
+	# check that we have at least one command
+	if [[ $# -ge 1 ]]
+	then
+		case "$1" in
+			start)  start;;
+			stop)   shift
+	        	stop "$@";;
+			status) shift
+	       		status "$@";;
+			*) usage
+	  			exit $INCORRECT_FIRST_ARGUMENT;;
+		esac
+	else
+		usage
+		exit $INCORRECT_FIRST_ARGUMENT
+	fi
+	exit $SUCCESS
+}
+
+main "$@"
---
+++ src/main/groovy/bpipe/executor/PbsproCommandExecutor.groovy
@@ -0,0 +1,179 @@
+/*
+* Copyright (c) 2013, davide.rambaldi@gmail.com
+*
+* Permission is hereby granted, free of charge, to any person
+* obtaining a copy of this software and associated documentation
+* files (the "Software"), to deal in the Software without
+* restriction, including without limitation the rights to use,
+* copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following
+* conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+* THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package bpipe.executor
+
+import groovy.util.logging.Log
+import bpipe.ForwardHost;
+import bpipe.PipelineError
+import bpipe.Utils
+
+/**
+ * Implementation of support for PBS Professional resource manager.
+ * <p>
+ * The actual implementation is a shell script written by 
+ * Davide Rambaldi <davide.rambaldi@gmail.com>.  This is just a 
+ * wrapper class that provides access to it via the
+ * CustomJob parent class support for shell script job
+ * managers.
+ * 
+ * @author davide.rambaldi@gmail.com
+ */
+@Mixin(ForwardHost)
+@Log
+class PbsproCommandExecutor extends CustomCommandExecutor implements CommandExecutor {
+
+    public static final long serialVersionUID = 0L
+
+    /**
+     * Constructor
+     */
+    PbsproCommandExecutor() {
+        super(new File(System.getProperty("bpipe.home") + "/bin/bpipe-pbspro.sh"))
+    }
+
+    /**
+     * Adds forwarding of standard err & out for processes started using Torque.
+     * These appear as files in the local directory.
+     */
+    @Override
+    public void start(Map cfg, String id, String name, String cmd, File outputDirectory) {
+        
+        // super.start(cfg, id, name, cmd, outputDirectory);
+
+        this.config = cfg
+        this.name = name
+        
+        log.info "Executing command using custom command runner ${managementScript}:  ${Utils.truncnl(cmd,100)}"
+        ProcessBuilder pb = new ProcessBuilder("bash", managementScript, "start")
+        Map env = pb.environment()
+        
+        // Environment variables that can be used to transmit 
+        // essential information
+        env.NAME = name
+        
+        this.jobDir = ".bpipe/commandtmp/$id"
+        File jobDirFile = new File(this.jobDir)
+        if(!jobDirFile.exists())
+            jobDirFile.mkdirs()
+        env.JOBDIR = jobDirFile.absolutePath
+        
+        env.COMMAND = '('+ cmd + ') > .bpipe/commandtmp/'+id+'/'+id+'.out 2>  .bpipe/commandtmp/'+id+'/'+id+'.err'
+        
+        // Davide Rambaldi: write to env the positon of PBS log files
+        env.PBSOUTPUT = jobDirFile.absolutePath + "/${id}.pbs.log"
+        env.PBSERROR = jobDirFile.absolutePath + "/${id}.pbs.err.log"
+
+        // If an account is specified by the config then use that
+        log.info "Using account: $config?.account"
+        if(config?.account)
+            env.ACCOUNT = config.account
+        
+        if(config?.walltime) 
+            env.WALLTIME = config.walltime
+            
+        if(config?.queue) 
+            env.QUEUE = config.queue
+        
+        if(config?.project)
+            env.PROJECT = config.project
+
+        // instead of resources we have a select statement
+        if(config?.select_statement)
+            env.SELECT_STATEMENT = config.select_statement
+
+        String startCmd = pb.command().join(' ')
+        log.info "Starting command: " + startCmd
+        
+        withLock(cfg) {
+            Process p = pb.start()
+            Utils.withStreams(p) {
+                StringBuilder out = new StringBuilder()
+                StringBuilder err = new StringBuilder()
+                p.waitForProcessOutput(out, err)
+                int exitValue = p.waitFor()
+                if(exitValue != 0) {
+                    reportStartError(startCmd, out,err,exitValue)
+                    throw new PipelineError("Failed to start command:\n\n$cmd")
+                }
+                this.commandId = out.toString().trim()
+                if(this.commandId.isEmpty())
+                    throw new PipelineError("Job runner ${this.class.name} failed to return a job id despite reporting success exit code for command:\n\n$startCmd\n\nRaw output was:[" + out.toString() + "]")
+                    
+                log.info "Started command with id $commandId"
+            }
+        }
+
+        // After starting the process, we launch a background thread that waits for the error
+        // and output files to appear and then forward those inputs
+
+        // FIXME; I don't see any forward to stderr and stdout
+        // Seems due to Forwarder.groovy class that at line 90 can't find the files ... 
+        // let's try to force file creation
+        new File(jobDirFile.absolutePath+"/${id}.out").createNewFile()
+        new File(jobDirFile.absolutePath+"/${id}.err").createNewFile()
+
+        forward(jobDirFile.absolutePath+"/${id}.out", System.out)
+        forward(jobDirFile.absolutePath+"/${id}.err", System.err)
+    }
+
+    /**
+     * Adds custom cleanup of pbspro created files and stop any threads forwarding output 
+     */
+    @Override
+    public void stop() {
+        super.stop();
+        cleanup()
+    }
+
+	void cleanup() {
+		this.forwarders*.cancel()
+		File of = new File(this.name+".o"+this.commandId)
+		if(of.exists())
+			of.delete()
+		File ef = new File(this.name+".e"+this.commandId)
+		if(ef.exists())
+			ef.delete()
+	}
+
+    /**
+     * The pbspro script / system produces files like BpipeJob.o133722 and 
+     * BpipeJob.e133722 that contain standard output and error.  We don't want
+     * these to be considered as result files from jobs so return a mask
+     * that screens them out.
+     * Note that:
+     * PBS pro -N name have the following specs:
+     * Format: string, up to 15  characters  in  length.   
+     * We trim in bpipe-pbspro.sh the job name to first 15 chars then we must trim also here
+     */
+    List<String> getIgnorableOutputs() {
+        def trimmed = this.name.size() > 15 ? this.name[0..14] : this.name
+        // println "Ignorable inputs trimmed are: " + trimmed + '.o.*$' + " and "  + trimmed + '.e.*$'
+        return [ trimmed + '.o.*$', trimmed + '.e.*$' ]
+    }
+
+    String toString() {
+        "Pbs Pro Job [" + super.toString() + "]"
+    }
+}
