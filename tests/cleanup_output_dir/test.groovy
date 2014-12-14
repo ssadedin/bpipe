ab3b8abcb19ffcb5f785e826b1f77908acb2040b
Simon Sadedin <ssadedin@gmail.com>
2014-12-15 09:58:21 +1100

    fix merge conflicts

---
+++ tests/cleanup_output_dir/test.groovy
@@ -0,0 +1,42 @@
+/*
+ * A simple pipeline with some intermediate files to test
+ * cleanup. Note that the last output (xls) depends only on the output
+ * of the first stage (html), but not on the intermediate ones (csv, xls).
+ * So we should be able to update the csv,xls files without actually requiring a 
+ * rebuild of the last stage.
+ */
+@transform("html")
+hello = {
+	exec "echo execute1;cp $input $output"
+        Thread.sleep(1100)
+}
+
+@transform("csv")
+there = {
+    output.dir = "there_output"
+	exec "echo execute2;cp $input $output"
+        Thread.sleep(1100)
+}
+
+@transform("xml")
+world = {
+	exec "echo execute3;cp $input $output"
+        Thread.sleep(1100)
+}
+
+@transform("xls")
+take = {
+  from("html") {
+    exec "echo execute4;cp $input $output"
+    Thread.sleep(1100)
+  }
+}
+
+@preserve
+me = {
+  exec "echo execute5;echo me > $output"
+}
+
+run {
+	hello + there + world + take + me
+}
