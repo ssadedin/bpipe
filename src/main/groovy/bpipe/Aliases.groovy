79c713ab92846f39c3072a39269e6edb2c1bb9f1
Simon Sadedin <ssadedin@gmail.com>
2015-05-25 09:29:46 +1000

    Prevent cleanup of any path involved in an alias as either target or source

--- src/main/groovy/bpipe/Aliases.groovy
+++ src/main/groovy/bpipe/Aliases.groovy
@@ -4,10 +4,17 @@ import groovy.transform.CompileStatic;
 
 class Aliases {
     
-    Map<String,String> aliases = [:]
+    private Map<String,String> aliases = [:]
+    
+    private HashSet<String> aliasTargets = new HashSet()
+    
+    synchronized boolean isAliased(String path) {
+        aliases.containsKey(path) || aliasTargets.contains(path)
+    }
     
     synchronized add(String fromPath, String toPath) {
         aliases[fromPath] = toPath
+        aliasTargets.add(toPath)
     }
     
     @CompileStatic
@@ -16,5 +23,9 @@ class Aliases {
         if(result)
             return result
         return fromPath
+    }
+    
+    synchronized String toString() {
+        aliases.toString()
     }
 }
