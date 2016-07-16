6f30112bda78aa4e7384a7dd925911f3e82fc0bb
Simon Sadedin <ssadedin@gmail.com>
2016-07-16 14:49:39 +1000

    fix empty_parallel_segment test hanging forever

--- tests/empty_parallel_segment/run.sh
+++ tests/empty_parallel_segment/run.sh
@@ -5,6 +5,6 @@ run
 
 grep -q 'Pipeline Succeeded' test.out || err "Pipeline failed with empty parallelisation list"
 
-grep -q 'Parallel segment will not execute because the list to parallelise over is empty' || err "Expected warning about empty list not printed"
+grep -q 'Parallel segment will not execute because the list to parallelise over is empty' test.out || err "Expected warning about empty list not printed"
 
 true
