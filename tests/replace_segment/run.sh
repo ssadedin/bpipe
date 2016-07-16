123f8a39775cb2721f60008ac00b7b1e908912b3
Simon Sadedin <ssadedin@gmail.com>
2016-07-16 16:50:02 +1000

    fix bad test failing incorrectly

--- tests/replace_segment/run.sh
+++ tests/replace_segment/run.sh
@@ -2,7 +2,7 @@ source ../testsupport.sh
 
 run
 
-grep -q "Stage stupid .jupiter." test.out || err "Failed to find output from stage replaced by branch"
+grep -q "Stage stupid" test.out || err "Failed to find output from stage replaced by branch"
 
 grep -q "^stupid" test.out || err "Failed to find expected word from stage replaced by branch"
 grep -q "^jupiter" test.out || err "Failed to find expected word from stage replaced by branch"
