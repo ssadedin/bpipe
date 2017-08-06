de10620343fbc670a5405055ba9edb1ee5123c6c
Simon Sadedin <ssadedin@gmail.com>
2017-08-07 08:31:57 +1000

    fix test failure due to altered output

--- tests/perchromosome_filterinputs/run.sh
+++ tests/perchromosome_filterinputs/run.sh
@@ -2,7 +2,7 @@ source ../testsupport.sh
 
 run *.bam
 
-[ `grep -c "Using bam file test.chr1.bam" test.out` == 2 ] || err "Incorrect number of references to test.chr1.bam"
+[ `grep -i -c "Using bam file test.chr1.bam" test.out` == 2 ] || err "Incorrect number of references to test.chr1.bam"
 
 true
 
