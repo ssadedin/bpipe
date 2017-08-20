d9d98cffa16716c4d54217a783e073d931da69a1
Simon Sadedin <ssadedin@gmail.com>
2017-08-20 21:35:32 +1000

    move ResourceUnit class into its own file

--- src/main/groovy/bpipe/Concurrency.groovy
+++ src/main/groovy/bpipe/Concurrency.groovy
@@ -38,46 +38,6 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import groovy.util.logging.Log;
 
-
-/**
- * A resource that can be managed by Bpipe.
- * The key is the name of the resource (eg: "memory") 
- * and the amount is the amount that is being used by  particular
- * operation.
- * <p>
- * Note: see {@link PipelineBodyCategory} for the magic that makes
- * these become referencable within pipline stages as "GB", 'threads',
- * and so on.
- * 
- * @author ssadedin
- */
-@Log
-class ResourceUnit implements Serializable {
-    
-    public static final long serialVersionUID = 0L
-    
-    public static final int UNLIMITED = -1
-    
-    int amount = 0;
-    
-    /**
-     * Note that the default max amount of 0 implies that the 
-     * maximum amount will be unlimited, or will take the limit
-     * imposed automatically by configuration
-     */
-    int maxAmount = 0
-    
-    String key
-    
-    String toString() {
-        "$amount $key"
-    }
-    
-    Object clone() {
-        new ResourceUnit(amount:amount, maxAmount:maxAmount, key:key)
-    }
-}
-
 class ResourceRequest implements Serializable {
     
     public static final long serialVersionUID = 0L
