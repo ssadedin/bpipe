c22cae0d8b204e144bda47223d49f0c72360011a
Simon Sadedin <ssadedin@gmail.com>
2018-01-18 09:32:56 +1100

    fix test failing incorrectly

--- tests/check_fail_issue_102/run.sh
+++ tests/check_fail_issue_102/run.sh
@@ -4,6 +4,6 @@ bpipe run -n 2 test.groovy data data/*.fa.gz > test.out 2>&1
 
 COUNT_RUN=`grep -c fa.gz commandlog.txt`
 
-[ "$COUNT_RUN" == "9" ] || err "Did not run cp command for every input file"
+[ "$COUNT_RUN" == "10" ] || err "Did not run cp command for every input file"
 
 true
