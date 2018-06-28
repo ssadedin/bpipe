c42d856334ac107df8686ea32009c9ba2558c75d
Simon Sadedin <ssadedin@gmail.com>
2018-06-28 10:41:59 +1000

    fix test failure due to diff 'cp -v' behavior

--- tests/forward_beats_output_same_type/run.sh
+++ tests/forward_beats_output_same_type/run.sh
@@ -3,6 +3,6 @@ source ../testsupport.sh
 
 run start.bam
 
-grep -q 'start.bam -> start.there.bam' test.out || err "Did not use correct input for there stage"
+grep -q 'start.bam.*->.*start.there.bam' test.out || err "Did not use correct input for there stage"
 
 true
