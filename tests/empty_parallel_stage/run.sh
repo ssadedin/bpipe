53b9edcf431e65d872f188f1fe768f96c054e056
Simon Sadedin <ssadedin@gmail.com>
2019-03-16 05:57:51 +0000

    fix test failing due to different output of cp on different platforms

--- tests/empty_parallel_stage/run.sh
+++ tests/empty_parallel_stage/run.sh
@@ -2,4 +2,4 @@ source ../testsupport.sh
 
 run test.vcf
 
-grep -q 'test.there.vcf -> test.there.world.vcf' test.out || err 'Incorrect file copied after empty parallel stage'
+grep -q '.*test.there.vcf.* ->.*test.there.world.vcf.*' test.out || err 'Incorrect file copied after empty parallel stage'
