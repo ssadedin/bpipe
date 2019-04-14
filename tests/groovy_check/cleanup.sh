6136da0e0eb708c6c6f742f6a6579b913b821258
Simon Sadedin <ssadedin@gmail.com>
2019-04-14 11:38:12 +1000

    fix regression : checks re-executing when unnecessary

---
+++ tests/groovy_check/cleanup.sh
@@ -0,0 +1,1 @@
+rm -rf .bpipe/outputs/  .bpipe/checks/
---
+++ tests/groovy_check/run.sh
@@ -0,0 +1,16 @@
+source ../testsupport.sh
+
+# Should fail check
+bpipe run -p a=1 test.groovy  > test.fail.out 2>&1
+
+grep -q 'Hmmm 1 <= 2' test.fail.out || err "Failed to see print from otherwise clause for failing check"
+
+rm hello.txt
+
+# Should pass check
+bpipe run -p a=10 test.groovy  > test.pass.out 2>&1
+
+grep -q 'Hmmm 10 <= 2' test.pass.out && err "Found print from otherwise clause even though check passed"
+
+
+
