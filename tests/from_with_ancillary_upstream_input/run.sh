b693d3e77124b351cc362b2b7e78effde88705ce
Simon Sadedin <ssadedin@gmail.com>
2019-06-10 19:30:38 +1000

    adjust test to better reflect fix

--- tests/from_with_ancillary_upstream_input/run.sh
+++ tests/from_with_ancillary_upstream_input/run.sh
@@ -2,6 +2,6 @@ source ../testsupport.sh
 
 run test.foo.txt test.bar.txt
 
-grep -q 'test.bar.there.bam correct=test.bar.there.bam' test.out || err "Incorrect input used: does not correspond to from"
+grep -q 'test.bar.there.bam -> ' test.out || err "Incorrect input used: does not correspond to from"
 
 true
