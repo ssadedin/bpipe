3e1f93425886453059d82827ed996a9ba9a940f3
Simon Sadedin <ssadedin@gmail.com>
2019-06-10 12:49:06 +0000

    fix os dependent behavior of cp causing test failures

--- tests/from_promotes_generic_input/run.sh
+++ tests/from_promotes_generic_input/run.sh
@@ -2,6 +2,6 @@ source ../testsupport.sh
 
 run test.txt
 
-grep -q 'test.txt -> something.txt' test.out || err 'File specified by from not used as default input'
+grep -q 'test.txt.*->.*something.txt' test.out || err 'File specified by from not used as default input'
 
 true
