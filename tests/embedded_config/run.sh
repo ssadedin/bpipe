54a82c763f2f2d259cff80b718d9803398bcd54d
Simon Sadedin <ssadedin@gmail.com>
2020-12-30 16:49:38 +1100

    fix fragile test failing due to reordered map keys

--- tests/embedded_config/run.sh
+++ tests/embedded_config/run.sh
@@ -4,7 +4,9 @@ source ../testsupport.sh
 bpipe test test.groovy > test.out
 
 
-grep -q 'executor:torque, memory:5g' test.out || err "Failed to find correct memory setting for torque"
+grep -q 'executor:torque' test.out || err "Failed to find correct executor setting for torque"
+
+grep -q 'memory:5g' test.out || err "Failed to find correct memory setting for command"
 
 
 true
