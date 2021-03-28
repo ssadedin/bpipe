7fa9b211c28794e8a2f8836d548e0e09f063352b
Simon Sadedin <ssadedin@gmail.com>
2021-03-28 08:16:14 +0000

    fix test broken on linux due to formatting of cp output

--- tests/multi_filetype/run.sh
+++ tests/multi_filetype/run.sh
@@ -2,13 +2,13 @@ source ../testsupport.sh
 
 bpipe run test.groovy test.tsv > test.out
 
-grep -q 'test.tsv -> test.xlsx' test.out || err "Copy of expected test.tsv output not observed"
+grep -q '.*test.tsv.* ->.*test.xlsx.*' test.out || err "Copy of expected test.tsv output not observed"
 
 rm -f test.xlsx
 
 bpipe run test.groovy test.csv > test.out
 
-grep -q 'test.csv -> test.xlsx' test.out || err "Copy of expected test.csv output not observed"
+grep -q '.*test.csv.* ->.*test.xlsx.*' test.out || err "Copy of expected test.csv output not observed"
 
 bpipe run test.groovy test.xml > test.out
 
