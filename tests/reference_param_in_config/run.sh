eb662cf38c7a1fb9907c9434f48e35c5a938ac13
Simon Sadedin <ssadedin@gmail.com>
2021-04-25 08:10:42 +1000

    better fail message for param test

--- tests/reference_param_in_config/run.sh
+++ tests/reference_param_in_config/run.sh
@@ -2,7 +2,7 @@ source ../testsupport.sh
 
 run
 
-grep -q 'Hello world Byebye' test.out || err 'Did not observe message with Hey'
+grep -q 'Hello world Byebye' test.out || err 'Did not observe message with Byebye from var in bpipe.config'
 
 bpipe run -p BAR=Hey test.groovy > test.out 2>&1
 
