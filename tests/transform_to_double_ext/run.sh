921c25cfcc7630eb0a3236a02c401962f96a360b
Simon Sadedin <ssadedin@gmail.com>
2021-04-27 12:05:09 +1000

    fix message format change causing test fail

--- tests/transform_to_double_ext/run.sh
+++ tests/transform_to_double_ext/run.sh
@@ -10,7 +10,7 @@ bpipe run test_fail.groovy test.bam > test2.out
 
 notexists test.median.txt 
 
-grep -q "ERROR: stage hello failed: An output containing or ending with '.foo' was referenced"  test2.out || err "Did not see expected error message"
+grep -q "foo,txt that is not compatible with those outputs"  test2.out || err "Did not see expected error message"
 
 true
 
