e108823941be677f03b533123639867fdf591473
Simon Sadedin <ssadedin@gmail.com>
2021-09-02 17:53:55 +1000

    make agent aware of existing pipeline in directory and wait for it to finish

--- src/main/groovy/bpipe/agent/AgentCommandRunner.groovy
+++ src/main/groovy/bpipe/agent/AgentCommandRunner.groovy
@@ -1,5 +1,7 @@
 package bpipe.agent
 
+import java.nio.channels.FileLock
+import java.nio.channels.OverlappingFileLockException
 import java.util.concurrent.Semaphore
 
 import java.util.logging.Logger
@@ -32,6 +34,11 @@ class AgentCommandRunner implements Runnable {
     
     AgentCommandCompletionListener completionListener
     
+    /**
+     * When waiting for acquisition of a lock, this object will be notified to interrupt
+     */
+    public static Object waitingForLockObject = new Object()
+    
     public AgentCommandRunner(WorxConnection worx, Long worxCommandId, BpipeCommand command) {
         super();
         this.worx = worx
@@ -53,9 +60,14 @@ class AgentCommandRunner implements Runnable {
         this.errorResponse = e
     }
     
+    static long MAX_LOCK_WAIT_TIME_MS = 300000L
+    
     @Override
     public void run() {
         
+        // Check for the run.pid file and try to lock it
+        checkRunningPipelineLock()
+           
         if(concurrency != null) {
             concurrency.acquire()
             log.info "Gained concurrency permit (${concurrency.availablePermits()} remaining) to run command $worxCommandId"
@@ -94,6 +106,9 @@ class AgentCommandRunner implements Runnable {
             if(concurrency != null) {
                 concurrency.release()
                 log.info "Released concurrency permit from command $worxCommandId (${concurrency.availablePermits()} now available)"
+                synchronized(waitingForLockObject) {
+                    waitingForLockObject.notifyAll()
+                }
             }
             
             if(this.completionListener != null) {
@@ -103,6 +118,63 @@ class AgentCommandRunner implements Runnable {
                     status: exitCode == 0 ? "ok" : "failed"
                 ])
             }
+        }
+    }
+
+    /**
+     * Look for a run.pid file from a running pipeline. If one is found, 
+     * test the lock on it to find out if the pipeline is still running. If so,
+     * wait with backoff before launching the pipeline
+     * 
+     * Note: this isn't 100% foolproof; two pipelines could still try to run at the 
+     * same time if race condition occurs. It should be very rare, but if it is a concern in that 
+     * case the risk can be addressed by running Bpipe in "queue" mode.
+     */
+    private void checkRunningPipelineLock() {
+        if(!(command instanceof RunPipelineCommand)) {
+            return
+        }
+
+        File runId = new File("$command.dir/.bpipe/lock")
+        if(!runId.exists()) {
+            return
+        }
+
+        log.info "Run id file for pipeline in $command.dir exists: checking lock available ..."
+        RandomAccessFile runIdRAF = new RandomAccessFile(runId, "rw")
+        FileLock lock = null
+        long waitTimeMs = 60000
+        try {
+            while(true) {
+                
+                try {
+                    lock = runIdRAF.channel.tryLock()
+                }
+                catch(OverlappingFileLockException olfe) {
+                    lock = null
+                }
+                
+                if(lock) {
+                    log.info "Successfully acquired lock in $command.dir : continuing to run pipeline"
+                    
+                    // This is here so that if multiple threads are waiting they all have a chance to reattempt the
+                    // file lock, ensuring that multiple threads should not grab the lock
+                    Thread.sleep(3000)
+
+                    break
+                }
+
+                log.info "Directory $command.dir appears to have a running pipeline: waiting $waitTimeMs to retry"
+                synchronized(waitingForLockObject) {
+                    waitingForLockObject.wait(waitTimeMs)
+                }
+                
+                waitTimeMs = Math.min(waitTimeMs*2, MAX_LOCK_WAIT_TIME_MS)
+            }
+        }
+        finally {
+            if(lock != null)
+                lock.close()
         }
     }
     
