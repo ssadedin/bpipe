5f082c625d189122b0f4564c7619d44366de3cbe
Simon Sadedin <ssadedin@gmail.com>
2022-10-08 22:07:25 +1100

    fix input splitter test

--- test-src/bpipe/InputSplitterTest.groovy
+++ test-src/bpipe/InputSplitterTest.groovy
@@ -2,6 +2,7 @@ package bpipe;
 
 import static org.junit.Assert.*;
 
+import bpipe.storage.LocalPipelineFile
 import org.junit.Test;
 
 class InputSplitterTest {
@@ -17,12 +18,12 @@ class InputSplitterTest {
         def inputs = [
                       "test.chr22.csv",
                       "test.chr22.realign.bam"
-                     ]
+                     ].collect { new LocalPipelineFile(it)}
         
         def pattern = ".chr%."
         def regex = splitter.convertPattern(pattern)
         println "regex = $regex"
-        def result = splitter.split(pattern,inputs)
+        def result = splitter.split(pattern,inputs).collectEntries { k,v -> [k, v*.path] }
         
         println "Got result:  $result"
         
@@ -46,7 +47,7 @@ class InputSplitterTest {
             "s_1_ignore_b_1.txt", "s_1_ignore_b_2.txt",
             "s_2_ignore_a_1.txt", "s_2_ignore_a_2.txt",
             "s_2_ignore_b_1.txt", "s_2_ignore_b_2.txt",
-           ]
+           ].collect { new LocalPipelineFile(it) }
         
        def pattern = "s_%_ignore_%_*.txt"
        
@@ -55,7 +56,7 @@ class InputSplitterTest {
        assert splitMap.pattern
        assert splitMap.splits.size() == 2
        
-       def result = splitter.split(pattern, inputs)
+       def result = splitter.split(pattern, inputs).collectEntries { k,v -> [k, v*.path] }
        
        assert result == [
             "1.a" : ["s_1_ignore_a_1.txt", "s_1_ignore_a_2.txt"],
@@ -93,7 +94,7 @@ class InputSplitterTest {
     @Test
     void testLeadingCommonPrefix() {
         
-	   def inputs = [ "11D_F_H14.txt", "11D_H14.txt", "11D_M_H14.txt" ]
+	   def inputs = [ "11D_F_H14.txt", "11D_H14.txt", "11D_M_H14.txt" ].collect { new LocalPipelineFile(it) }
         
        def pattern = "%_H"
        
@@ -118,8 +119,8 @@ class InputSplitterTest {
 	@Test
 	public void testSplit() {
         
-		def inputs = ["s_1_1.txt", "s_1_2.txt", "s_2_2.txt", "s_2_1.txt", "foo.bar"]
-        def result = splitter.split("s_%_*.txt",inputs) 
+		def inputs = ["s_1_1.txt", "s_1_2.txt", "s_2_2.txt", "s_2_1.txt", "foo.bar"].collect { new LocalPipelineFile(it) }
+        def result = splitter.split("s_%_*.txt",inputs) .collectEntries { k,v -> [k, v*.path] }
         assert result == [ 
 		    "1": ["s_1_1.txt", "s_1_2.txt"],
             "2": ["s_2_1.txt", "s_2_2.txt"]
@@ -136,11 +137,11 @@ class InputSplitterTest {
                       "SureSelect_Capture_11MG2107_AD0AN0ACXX_GATCAG_L002_R2.fastq",
                       "SureSelect_Capture_11MG2108_AD0AN0ACXX_TAGCTT_L002_R1.fastq",
                       "SureSelect_Capture_11MG2108_AD0AN0ACXX_TAGCTT_L002_R2.fastq"
-                     ]
+                     ].collect { new LocalPipelineFile(it) }
         
         def regex = splitter.convertPattern("CXX_%_*.fastq")
         println "regex = $regex"
-        def result = splitter.split("CXX_%_*.fastq",inputs) 
+        def result = splitter.split("CXX_%_*.fastq",inputs).collectEntries { k,v -> [k, v*.path] }
         
         println "Got result:  $result"
         
@@ -160,12 +161,12 @@ class InputSplitterTest {
 		def inputs = [
                       "120206Bha_D12-530_1_sequence.fastq",
                       "120206Bha_D12-530_2_sequence.fastq"
-                     ]
+                     ].collect { new LocalPipelineFile(it) }
         
         def pattern = "530_%_sequence.fastq"
         def regex = splitter.convertPattern(pattern)
         println "regex = $regex"
-        def result = splitter.split(pattern,inputs) 
+        def result = splitter.split(pattern,inputs).collectEntries { k,v -> [k, v*.path] }
         
         println "Got result:  $result"
         
@@ -184,13 +185,13 @@ class InputSplitterTest {
                       "temp_Blasto_female_1_Ac02g4acxx_CAGATC_L003_R1.fastq_unpaired",
                       "temp_Blasto_female_1_Ac02g4acxx_CAGATC_L003_R2.fastq_paired",
                       "temp_Blasto_female_1_Ac02g4acxx_CAGATC_L003_R2.fastq_unpaired"
-                     ]
+                     ].collectNested { new LocalPipelineFile(it) }
         
         def pattern = "Blasto_%_L*.fastq_paired"
         def regex = splitter.convertPattern(pattern)
         
         println "regex = $regex"
-        def result = splitter.split(pattern,inputs) 
+        def result = splitter.split(pattern,inputs).collectEntries { k,v -> [k, v*.path] }
         
         println "Got result:  $result"
         assert result == [
@@ -212,9 +213,9 @@ class InputSplitterTest {
 					 "s_1_3.txt",
 //					 "s_1_17.txt",
 					 "s_1_7.txt"
-                 ]
+                 ].collect { new LocalPipelineFile(it) }
 			
-        def result = splitter.sortNumericThenLexically(~"_([^_]*)_(.*)",[0], l1)
+        def result = splitter.sortNumericThenLexically(~"_([^_]*)_(.*)",[0], l1)*.path
         assert result == [
 		        	 "s_1_1.txt",
 					 "s_1_2.txt",
@@ -273,7 +274,8 @@ class InputSplitterTest {
     
     @Test
     void testNumericMatch() {
-        def result = splitter.split("%_R#.gz",["test1_R1.gz", "test1_R2.gz", "test2_RX_R1.gz", "test2_RX_R2.gz"])
+        def files = ["test1_R1.gz", "test1_R2.gz", "test2_RX_R1.gz", "test2_RX_R2.gz"].collect { new LocalPipelineFile(it) }
+        def result = splitter.split("%_R#.gz",files).collectEntries { k,v -> [k, v*.path] }
         assert result == [ 
             "test1" : ["test1_R1.gz", "test1_R2.gz"],
             "test2_RX" : ["test2_RX_R1.gz", "test2_RX_R2.gz"]
@@ -289,7 +291,9 @@ class InputSplitterTest {
     
     @Test 
     void testWithPattern() {
-        def result = splitter.split(~"(.*)_R[0-9]*.gz",["test1_R1.gz", "test1_R2.gz", "test2_RX_R1.gz", "test2_RX_R2.gz"])
+        
+        def files = ["test1_R1.gz", "test1_R2.gz", "test2_RX_R1.gz", "test2_RX_R2.gz"].collect { new LocalPipelineFile(it) }
+        def result = splitter.split(~"(.*)_R[0-9]*.gz",files).collectEntries { k,v -> [k, v*.path]  }
         assert result == [ 
             "test1" : ["test1_R1.gz", "test1_R2.gz"],
             "test2_RX" : ["test2_RX_R1.gz", "test2_RX_R2.gz"]
