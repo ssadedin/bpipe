da516bb7a90cd9e5b4ccac42db39fc65db8ff33c
Simon Sadedin <ssadedin@gmail.com>
2023-04-29 10:51:10 +1000

    switch container runtimes to use /bin/bash as default shell, but make shell configurable
    because:
      - /bin/sh gives errors for `set -o pipefail` in some containers,
        but omitting this would not be acceptable as a default

--- src/main/groovy/bpipe/processors/DockerContainerWrapper.groovy
+++ src/main/groovy/bpipe/processors/DockerContainerWrapper.groovy
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2012-2023 MCRI, authors
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package bpipe.processors
 
 import bpipe.*
@@ -5,6 +29,11 @@ import bpipe.storage.BindStorageLayer
 import bpipe.storage.StorageLayer
 import groovy.util.logging.Log
 
+/**
+ * A wrapper that adapts commands to run inside a docker container
+ * 
+ * @author simon.sadedin
+ */
 @Log
 class DockerContainerWrapper implements CommandProcessor {
 
@@ -13,6 +42,8 @@ class DockerContainerWrapper implements CommandProcessor {
 		
         Map config = command.processedConfig.container
         
+        String shell = config.getOrDefault('shell', '/bin/bash')
+
         def extraVolumes = Config.listValue(config, 'storage').collect { name ->
             
             BindStorageLayer layer = (BindStorageLayer)StorageLayer.create(name)
@@ -25,7 +56,7 @@ class DockerContainerWrapper implements CommandProcessor {
         
 		log.info("Volume arguments for docker command: ${extraVolumes}")
 		
-		List<String> dockerCommand = [config.getOrDefault('command', ['/bin/sh','-e'])].flatten()
+		List<String> dockerCommand = [config.getOrDefault('command', [shell,'-e'])].flatten()
 
 		String entryPoint = config.getOrDefault('entryPoint', null)
 		List<String> entryPointArg
