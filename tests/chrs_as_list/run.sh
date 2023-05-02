22d02cd618b46fe1e419c2998fa4ec7fe0f4bdd4
Simon Sadedin <ssadedin@gmail.com>
2023-05-02 20:51:48 +1000

    add small wait to decrease probability of race condition preventing test output, causing failure

--- tests/chrs_as_list/run.sh
+++ tests/chrs_as_list/run.sh
@@ -12,7 +12,7 @@ bpipe run -L chr2 test.groovy > test.out 2>&1
 
 grep -q 'Processing chr1' test.out &&  err 'Should not have processed chr1'
 
-grep -q 'Processing chr2' test.out || err 'Should have processed chr2'
+grep -q 'Processing chr2' test.out || err 'Should have processed chr2 when only chr2 included'
 
 true
 
--- tests/chrs_as_list/test.groovy
+++ tests/chrs_as_list/test.groovy
@@ -4,6 +4,9 @@ hello = {
     exec """
         echo "Processing $chr"
     """
+
+    Thread.sleep(1000) // ensure that output has time to spool before process ends
+                       // was causing sporadic test failures due to no output
 }
 
 run {
