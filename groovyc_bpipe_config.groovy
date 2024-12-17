3393b720171da4017eb3b0bcbb6768082b465c05
Simon Sadedin <ssadedin@gmail.com>
2024-12-17 15:26:00 +1100

    add default imports to custom compiler config script

--- groovyc_bpipe_config.groovy
+++ groovyc_bpipe_config.groovy
@@ -1,3 +1,9 @@
+
+
 withConfig(configuration) {
     configuration.setScriptBaseClass('bpipe.BpipeScriptBase')
+    configuration.addCompilationCustomizers(new org.codehaus.groovy.control.customizers.ImportCustomizer().tap {
+        addImport('Filter', 'Filter')
+        addImport('Transform', 'Transform')
+    })
 }
