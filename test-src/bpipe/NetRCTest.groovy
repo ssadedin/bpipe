71959c93fc01913fb8ccef103efa9ed407c88de3
Simon Sadedin <simon.sadedin@mcri.edu.au>
2025-06-01 08:57:06 +1000

    fix wrong imports for netrc test

--- test-src/bpipe/NetRCTest.groovy
+++ test-src/bpipe/NetRCTest.groovy
@@ -1,8 +1,8 @@
 package bpipe
 
-import static org.junit.jupiter.api.Assertions.*
+import static org.junit.Assert.*
 
-import org.junit.jupiter.api.Test
+import org.junit.Test
 
 class NetRCTest {
 
