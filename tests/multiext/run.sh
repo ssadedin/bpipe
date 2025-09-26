3e9374e1db5e4133974ba73fce5c74c7b655931d
Simon Sadedin <simon.sadedin@mcri.edu.au>
2025-09-26 20:23:02 +1000

    fix bad pattern for test match

--- tests/multiext/run.sh
+++ tests/multiext/run.sh
@@ -2,7 +2,7 @@ source ../testsupport.sh
 
 run
 
-grep -q 'foo.vcf.gz .* foo.vcf.world.txt' test.out || err "Selected wrong output from produce when only partial suffix matches"
+grep -q 'foo.vcf.gz.*foo.vcf.world.txt' test.out || err "Selected wrong output from produce when only partial suffix matches"
 
 exists foo.vcf.world.txt
 
