a3ec1910aea424b2fc33ef4088d438d62a5c2415
Simon Sadedin <ssadedin@gmail.com>
2026-05-03 17:10:31 +1000

    another attempt at fixing quiet_and_clean test
    fixing race conditions

--- tests/quiet_and_clean/run.sh
+++ tests/quiet_and_clean/run.sh
@@ -2,7 +2,13 @@ source ../testsupport.sh
 
 bpipe run test.groovy > test.out 2>&1 &
 
-sleep 1
+# Wait for first bpipe to actually be running (run.pid is written after lock is acquired)
+for i in $(seq 1 30); do
+    if [ -f .bpipe/run.pid ]; then
+        break
+    fi
+    sleep 1
+done
 
 export BPIPE_QUIET=true
 
